34c5b9ed2929ebeba1a77db7de6cf2c88bc3f628
Helene <hrenninger@crimson.ua.edu>
2024-09-06 13:46:04 -0500

    Lab 2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//initialize variables
+int numberOfSandwiches = 0;
+int numberOfToppings = 0;
+double tip = 0;
+double totalSandwichCost;
+double totalToppingCost;
+double baseCost;
+
+//initialize constants
+const double COST_OF_SANDWICH = 4.75;
+const double COST_OF_TOPPING = 0.55;
+const double DISCOUNT_AMOUNT = 0.10;
+
+//prompt the user for # sandwiches
+System.Console.WriteLine("How many sandwiches?");
+numberOfSandwiches = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Sandwiches "+numberOfSandwiches);
